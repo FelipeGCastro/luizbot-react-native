5844943b97a6c2054428a69e243d03dd1fd476fd
Luiz Felipe Castro <felipe1693@gmail.com>
2021-08-16 19:39:05 +0100

    set heroku local to api

--- src/services/api.js
+++ src/services/api.js
@@ -2,8 +2,7 @@ import axios from 'axios'
 
 // Create axios client, pre-configured with baseURL
 const api = axios.create({
-  baseURL: 'http://192.168.1.81:3333'
-  // baseURL: 'https://luiz-binance-bot.herokuapp.com'
+  baseURL: 'https://luiz-binance-bot.herokuapp.com'
 })
 
 // Set JSON Web Token in Client to be included in all calls
